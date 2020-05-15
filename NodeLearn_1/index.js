742f19a8a90c17daee7d34e01b5b0fe9441bc76d
KlerKpn <59790939+KlerKpn@users.noreply.github.com>
2020-05-15 06:13:57 +0300

    day 2
    be careful

--- NodeLearn_1/index.js
+++ NodeLearn_1/index.js
@@ -1,4 +1,39 @@
-const userObj = require( "./users");
-console.log(userObj.user)
+const http =  require("http")
 
-userObj.fun()
+const server = http.createServer((req, res)=>{
+    if(req.method === "GET"){
+        res.writeHead(200, {
+            "Content-Type": "text/html"
+        })
+        res.end(`
+        <h1>Form</h1>
+        <form method="POST" action="/">
+            <input name="title" type="text" >
+            <button type="submit" >send</button>
+        </form>
+        `)
+    } else if (req.method === "POST"){
+        const body = []
+        res.writeHead(200, {
+            "Content-Type": "text/html; charset=utf-8"
+        })
+        req.on('data', data => {
+            body.push(Buffer.from(data))
+        })
+
+        req.on('end', ()=>{
+            const message = (body.toString().split("=")[1])
+
+                res.end(`
+            <h1>Жопа: ${message} </h1>
+            
+            `)
+        })
+        
+        
+    }
+    
+})
+server.listen(3000, ()=>{
+    console.log("server is running")
+})
