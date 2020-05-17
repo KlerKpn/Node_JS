42d96affb73de6168ee8a6f6f9b1c9a17b2f4785
KlerKpn <59790939+KlerKpn@users.noreply.github.com>
2020-05-17 10:22:14 +0300

    Express-handlebars
    join handlebars $ materializecss

--- Node_Express/index.js
+++ Node_Express/index.js
@@ -1,17 +1,40 @@
 const express = require('express')
 const path = require('path')
+const exphbs = require('express-handlebars')
 
 const app = express()
 
-app.get('/', (req, res)=>{
-    res.status(200)
-    res.sendFile(path.join(__dirname, 'views', './index.html'))
-} )
-
-app.get('/about', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'views', './about.html'))
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
 })
 
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res)=>{
+    res.render('index',{
+        title: 'Home',
+        isHome: true
+    })
+} )
+
+app.get('/items', (req, res)=>{
+    res.render('items',{
+        title: 'Продукты',
+        isItems: true
+    })
+})
+
+app.get('/add', (req, res)=>{
+    res.render('add',{
+        title: 'Новый продукт',
+        isAdd: true
+    })
+})
 const PORT = process.env.PORT || 3000
 app.listen(PORT, ()=>{
     console.log(`server is running on port ${PORT}`)
